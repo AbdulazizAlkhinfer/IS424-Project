c49a2a8e2da9a0b331b5aab343386f397d895c2f
AbdulazizAlkhinfer <3zoozyfn@gmail.com>
2024-11-25 05:14:34 +0300

    update-model.py

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -8,5 +8,13 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField()
     expiration_date = models.DateField()
     picture = models.URLField(null=True)
+class PharmacyInstance(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=128) 
     def __str__(self):
         return self.name
+    
+
+    
